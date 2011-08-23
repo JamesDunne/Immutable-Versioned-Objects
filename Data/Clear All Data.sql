2cc9f38c5571f09bb40df0d1bae6c5606da745af
James Dunne <james.jdunne@gmail.com>
2011-08-23 11:30:13 -0500

    Cleared all data out.

---
+++ Data/Clear All Data.sql
@@ -0,0 +1,15 @@
+USE [GitCMS];
+GO
+
+-- disable all constraints
+EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+
+-- delete data in all tables
+EXEC sp_MSForEachTable "DELETE FROM ?"
+
+-- enable all constraints
+exec sp_msforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
+
+-- no identity columns
+--EXEC sp_MSforeachtable "DBCC CHECKIDENT ( '?', RESEED, 0)"
+GO
