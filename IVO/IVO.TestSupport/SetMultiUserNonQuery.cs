01766eb2db8db52cd411a8cbde9158fbbb19a05d
James Dunne <james.jdunne@gmail.com>
2011-09-12 19:47:10 -0500

    Cleaned up SQL unit test pattern. DeleteBlobsTest is still failing with network connection issue. I suspect it is SQL Express denying connection with too much data in the @contents parameter for the PersistBlob query.

--- IVO/IVO.TestSupport/SetMultiUserNonQuery.cs
+++ IVO/IVO.TestSupport/SetMultiUserNonQuery.cs
@@ -8,21 +8,13 @@ using System.Data.SqlClient;
 
 namespace IVO.TestSupport
 {
-    public sealed class ClearAllDataNonQuery : IDataOperation<int>
+    public sealed class SetMultiUserNonQuery : IDataOperation<int>
     {
         public SqlCommand ConstructCommand(SqlConnection cn)
         {
-            // WARNING: this deletes ALL data from ALL tables in the target database!
             var cmd = new SqlCommand(
                 String.Format(
-@"-- disable all constraints
-EXEC sp_msforeachtable ""ALTER TABLE ? NOCHECK CONSTRAINT all""
-
--- delete data in all tables
-EXEC sp_MSForEachTable ""DELETE FROM ?""
-
--- enable all constraints
-EXEC sp_msforeachtable ""ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all""",
+@"EXEC sp_dboption '{0}', 'single user', 'FALSE';",
                     cn.Database
                 ),
                 cn
