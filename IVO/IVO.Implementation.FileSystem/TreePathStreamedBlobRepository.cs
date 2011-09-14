2ee9bec2775bfc6315b39e6db0df471f95c96998
James Dunne <james.jdunne@gmail.com>
2011-09-14 08:37:44 -0500

    Implemented TreePathStreamedBlobRepository.

--- IVO/IVO.Implementation.FileSystem/TreePathStreamedBlobRepository.cs
+++ IVO/IVO.Implementation.FileSystem/TreePathStreamedBlobRepository.cs
@@ -21,9 +21,42 @@ namespace IVO.Implementation.FileSystem
             this.blrepo = blrepo ?? new StreamedBlobRepository(system);
         }
 
-        public Task<TreePathStreamedBlob[]> GetBlobsByTreePaths(params TreeBlobPath[] treePaths)
+        public async Task<TreePathStreamedBlob[]> GetBlobsByTreePaths(params TreeBlobPath[] treePaths)
         {
-            throw new NotImplementedException();
+#if false
+            var blobs =
+                from tp in treePaths
+                let tr = await trrepo.GetTreeIDByPath(new TreeTreePath(tp.RootTreeID, tp.Path.Tree))
+            // await operator cannot currently be used in query expressions.
+#else
+            // Estimated size:
+            TreePathStreamedBlob[] blobs = new TreePathStreamedBlob[treePaths.Length];
+            
+            for (int i = 0; i < treePaths.Length; ++i)
+            {
+                var tp = treePaths[i];
+                
+                var trm = await trrepo.GetTreeIDByPath(new TreeTreePath(tp.RootTreeID, tp.Path.Tree));
+                if (!trm.TreeID.HasValue)
+                {
+                    blobs[i] = null;
+                    continue;
+                }
+
+                // Get the tree:
+                var tr = await trrepo.GetTree(trm.TreeID.Value);
+                
+                // Get the blob out of this tree:
+                // TODO: standardize name comparison semantics:
+                var trbl = tr.Blobs.SingleOrDefault(x => x.Name == tp.Path.Name);
+                
+                // Finally return the streamed blob:
+                // TODO: unknown length of blob; is it a problem?
+                blobs[i] = new TreePathStreamedBlob(tp, new StreamedBlob(blrepo, trbl.BlobID));
+            }
+
+            return blobs;
+#endif
         }
     }
 }
