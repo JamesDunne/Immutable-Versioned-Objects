faeadb93b6c6be5f0b2a03e43d99f36e8de4f930
James Dunne <james.jdunne@gmail.com>
2011-09-17 22:23:14 -0500

    Added TypeConverter for TreeBlobPath to be MVC-friendly.

--- IVO/IVO.Definition/Models/TreeBlobPath.cs
+++ IVO/IVO.Definition/Models/TreeBlobPath.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace IVO.Definition.Models
 {
     /// <summary>
     /// Represents a canonical path to a blob relative to a root TreeID.
     /// </summary>
+    [TypeConverter(typeof(TreeBlobPathTypeConverter))]
     public sealed class TreeBlobPath
     {
         public TreeBlobPath(TreeID rootTreeID, CanonicalBlobPath path)
@@ -18,5 +20,35 @@ namespace IVO.Definition.Models
 
         public TreeID RootTreeID { get; private set; }
         public CanonicalBlobPath Path { get; private set; }
+    }
+
+    public sealed class TreeBlobPathTypeConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (typeof(string) == sourceType)
+                return true;
+            else
+                return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            string strValue = value as string;
+            if (strValue != null)
+            {
+                // {TreeID}{/CanonicalBlobPath}
+                int firstSlash = strValue.IndexOf('/');
+                if (firstSlash < 0) goto fail;
+                
+                TreeID root = new TreeID(strValue.Substring(0, firstSlash));
+                CanonicalBlobPath path = (CanonicalBlobPath)strValue.Substring(firstSlash);
+                
+                return new TreeBlobPath(root, path);
+            }
+
+        fail:
+            return base.ConvertFrom(context, culture, value);
+        }
     }
 }
